6a5df84b029bc5783f05b402e4d03104762a1581
takumi2003 <takumi20030819@gmail.com>
2024-06-13 22:06:27 +0900

    feat: 元データウェーブレット係数表示

--- DWTModel.java
+++ DWTModel.java
@@ -1,6 +1,8 @@
+import WaveletData.WaveletData;
+
 class DWTModel {
     public static double[] transform(double[] data) {
-        // DWTの実装
-        return new double[] { 1,1,1,1,2,2,3,3,4,4,5,5,6,6,6,6 };
+        double[] coeffs = WaveletData.dataSampleCoefficients();
+        return coeffs;
     }
 }
